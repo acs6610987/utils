d02634e40de6fe50a4c221d3036a68fc7ae9f13d
Zhicong Huang <zhicong303@gmail.com>
2016-01-22 16:38:15 +0100

    Update store_NA18602.sh

--- shellutils/store_NA18602.sh
+++ shellutils/store_NA18602.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mysql -uhzc -pacs524400951 <<SCRIPT
+mysql -uhzc -pYOUR_PASSWORD <<SCRIPT
 use genomic_raw_data;
 create table encrypted_NA18602(position bigint, encrypted_cigar_content varbinary(128), random_salt varbinary(8));
 load data infile '/home/lca1/workspace/data/NA18602.mapped.ILLUMINA.bwa.CHB.low_coverage.20120522.bam.enc.dat' into table encrypted_NA18602 fields terminated by ',' optionally enclosed by '"' escaped by '\\\\' lines terminated by '\\n' (position, encrypted_cigar_content, random_salt);
